7a8cf8ba2463b208944d420c59d8f8437e3c4caa
vanstorm9 <antlowhur@yahoo.com>
2018-11-10 11:48:33 -0500

    Adding line script

--- test/experiment/shape-detection/detect_shapes.py
+++ test/experiment/shape-detection/detect_shapes.py
@@ -6,6 +6,7 @@ from pyimagesearch.shapedetector import ShapeDetector
 import argparse
 import imutils
 import cv2
+import numpy as np
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -16,22 +17,31 @@ args = vars(ap.parse_args())
 # load the image and resize it to a smaller factor so that
 # the shapes can be approximated better
 image = cv2.imread(args["image"])
-resized = imutils.resize(image, width=300)
+resized = imutils.resize(image, width=30)
 ratio = image.shape[0] / float(resized.shape[0])
 
 # convert the resized image to grayscale, blur it slightly,
 # and threshold it
 gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+#blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+blurred = cv2.Canny(gray,100,200)
+thresh = cv2.threshold(blurred, 157, 455, cv2.THRESH_BINARY)[1]
+#thresh = cv2.bitwise_not(blurred)
+
+cv2.imshow('test',thresh)
 
 # find contours in the thresholded image and initialize the
 # shape detector
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+#cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_TREE,
 	cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-sd = ShapeDetector()
 
+
+
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+sd = ShapeDetector()
+print len(cnts)
 # loop over the contours
 for c in cnts:
 	# compute the center of the contour, then detect the name of the
@@ -40,7 +50,7 @@ for c in cnts:
 	cX = int((M["m10"] / M["m00"]) * ratio)
 	cY = int((M["m01"] / M["m00"]) * ratio)
 	shape = sd.detect(c)
-
+	print shape
 	# multiply the contour (x, y)-coordinates by the resize ratio,
 	# then draw the contours and the name of the shape on the image
 	c = c.astype("float")
